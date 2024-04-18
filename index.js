22c67916b9af04aca75da897ed6ff98ea3621a1d
MuhammadSamad30 <romivlogs11@gmail.com>
2024-04-18 15:19:14 +0500

    word counter

---
+++ index.js
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.whiteBright("\nWelcome to the word counter from ") + chalk.greenBright("Muhammad Samad!"));
+console.log();
+async function main() {
+    const answers = await inquirer.prompt([
+        {
+            name: "sentence",
+            type: "input",
+            message: chalk.whiteBright("Enter a sentence: "),
+            validate: (input) => {
+                if (input.trim().length === 0) {
+                    return chalk.redBright("Please enter a valid sentence.");
+                }
+                return true;
+            }
+        }
+    ]);
+    const sentence = answers.sentence.trim();
+    const words = sentence.toLowerCase().split(/\s+/);
+    const optionPrompt = await inquirer.prompt([
+        {
+            name: "option",
+            type: "list",
+            message: chalk.whiteBright("Choose an option:"),
+            choices: ["Count Words", "Exit"]
+        }
+    ]);
+    if (optionPrompt.option === "Count Words") {
+        console.log(`Your number of words is: ${words.length}`);
+    }
+    if (optionPrompt.option === "Exit") {
+        console.log(chalk.yellowBright("Exiting program..."));
+        console.log(chalk.greenBright.italic("\n\tThanks for using this app"));
+        return;
+    }
+    const continuePrompt = await inquirer.prompt({
+        name: "continue",
+        type: "confirm",
+        message: chalk.whiteBright("Do you want to continue?")
+    });
+    if (continuePrompt.continue) {
+        console.log();
+        main();
+    }
+    else {
+        console.log(chalk.yellowBright("Exiting program..."));
+        console.log(chalk.greenBright.italic("\n\tThanks for using this app"));
+    }
+}
+main();
